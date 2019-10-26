81c7998e26d335ecec288f708c56380733bbc7de
hapjeong9 <hapjeong9@gmail.com>
2019-10-26 23:46:09 +0900

    welcom post updated

--- _posts/2019-10-26-welcome-to-sat9-blog.md
+++ _posts/2019-10-26-welcome-to-sat9-blog.md
@@ -11,12 +11,15 @@ Agile Saturday, 토요모임 2.0 Blog를 시작합니다
 
 ## 소개
 
-2016년 1월 9일부터 함께 공부하는 Agile Saturday 모임의 git repository입니다.
+2016년 1월 9일부터 함께 공부하는 Agile Saturday 모임의 blog입니다.
+함께 공부하며 만든 결과물을 블로그로 릴리즈합니다.
 
 ## 멤버
 
-- 이상폴
-- 합정동
-- 조엘
+모임의 멤버와 간단한 소개, 관심사들의 링크입니다.
+
+- [이상폴](https://github.com/hapjeong9/saturday9/wiki/%EC%9D%B4%EC%83%81%ED%8F%B4)
+- [합정동](https://github.com/hapjeong9/saturday9/wiki/%ED%95%A9%EC%A0%95%EB%8F%99)
+- [조엘](https://github.com/hapjeong9/saturday9/wiki/%EC%A1%B0%EC%97%98)
 - n0
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Agile Saturday, 토요모임 2.0 Blog를 시작합니다",
-        "excerpt":"Agile Saturday, 토요모임 2.0 Blog를 시작합니다   소개   2016년 1월 9일부터 함께 공부하는 Agile Saturday 모임의 git repository입니다.   멤버      이상폴   합정동   조엘   n0   ","categories": ["blog"],
+        "excerpt":"Agile Saturday, 토요모임 2.0 Blog를 시작합니다   소개   2016년 1월 9일부터 함께 공부하는 Agile Saturday 모임의 blog입니다. 함께 공부하며 만든 결과물을 블로그로 릴리즈합니다.   멤버   모임의 멤버와 간단한 소개, 관심사들의 링크입니다.      이상폴   합정동   조엘   n0   ","categories": ["blog"],
         "tags": ["sat9","update"],
         "url": "http://localhost:4000/blog/welcome-to-sat9-blog/",
         "teaser":null}]
